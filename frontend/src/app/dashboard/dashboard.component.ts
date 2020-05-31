ac2116d8cbc45f1143d188b36be9ce0242617f98
khirasindhu-pyrite <khirasindhu@pyritefitness.com>
2020-05-31 18:35:50 +0530

    dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from './dashboard.service';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
   dashboardList: any;
 
   constructor(private dashboardService : DashboardService) { }
 
   ngOnInit() {
-    // this.getDashboardArtistsList();
+    this.dashboardService.getArtistProfiles.subscribe(result => {
+      console.log("all_date", result.length)
+      this.dashboardList = result;
+    })
   }
-
-  // getDashboardArtistsList()
-  // {
-  //   this.dashboardService.getArtistProfiles().subscribe(
-  //     (res: any) =>
-  //     {
-  //       this.dashboardList = res;
-  //       console.log('success', res);
-  //     },
-  //     err=>{
-  //       console.log('error', err);
-  //     });
-  // }
 }
