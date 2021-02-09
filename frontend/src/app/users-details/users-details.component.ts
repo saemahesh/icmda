0a185a9b34637433f873d4a8e4ae1fe920e8de28
Sai Rambha <sai.rambha@factset.com>
2021-02-09 11:00:15 +0530

    dashboard ronak changes

--- frontend/src/app/users-details/users-details.component.ts
+++ frontend/src/app/users-details/users-details.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild,TemplateRef,ElementRef } from "@angular/core";
 import { UsersDetailsService } from "./users-details.service";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
-import { AbstractControl, FormBuilder } from "@angular/forms";
+import { AbstractControl } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from "@angular/platform-browser";
+
 @Component({
   selector: "app-users-details",
   templateUrl: "./users-details.component.html",
   styleUrls: ["./users-details.component.css"],
 })
 export class UsersDetailsComponent implements OnInit {
+  markForm: FormGroup;
+  submitted = false;
+  videourl:any="https://drive.google.com/file/d/1r4i52e7ihLV551aaABAvOpFksN0Z19yz/preview";
+
   displayedColumns: string[] = [
     "sNo",
     "name",
@@ -19,15 +26,19 @@ export class UsersDetailsComponent implements OnInit {
     "compLevel",
     "id",
     "picture",
+    "action"
   ];
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef; 
   readonly formControl: AbstractControl;
   userdetails: any;
   dataSource: any;
-
+  judgesdata:any;
+  selectedData:any;
   constructor(
-    public userservice: UsersDetailsService,
-    formBuilder: FormBuilder
+    public userservice: UsersDetailsService, 
+    private formBuilder: FormBuilder, 
+    private sanitizer: DomSanitizer
   ) {
     this.formControl = formBuilder.group({
       name: "",
@@ -50,6 +61,13 @@ export class UsersDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getData();
+    this.getJudges();
+    this.createForm();
+  }
+  getJudges(){
+    this.userservice.getjudges().subscribe((res)=>{     
+      this.judgesdata=res;      
+        })
   }
 
   getData() {
@@ -97,4 +115,59 @@ export class UsersDetailsComponent implements OnInit {
     this.formControl.reset();
     this.dataSource.filter = "";
   }
+     
+  createForm(){
+    this.markForm = this.formBuilder.group({
+      judgeName: ['', Validators.required],
+      mark: ['', Validators.required],    
+  });
+     
+  }
+
+  get f() { return this.markForm.controls; }
+
+view(data){
+  this.selectedData=data;
+  var lastSlash = this.selectedData.video_link.lastIndexOf("/");   
+  if(this.selectedData.video_link.includes('drive.google.com'))
+  {
+    this.videourl= this.selectedData.video_link.substring(0,lastSlash)+"/preview"; 
+  }else{
+    this.videourl= this.selectedData.video_link; 
+  }
+    
+}
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.markForm.invalid) {
+        return;
+    }
+
+   let data={
+    "participant_id":this.selectedData.id,
+    "judge_id":this.markForm.value.judgeName,
+    "marks":this.markForm.value.mark
+  }  
+  this.userservice.postMarks(data).subscribe((res)=>{
+    console.log("--------here",res)
+  },(error)=>{
+    console.log("-----------here is the error",error);
+  }
+  );
+}
+
+onReset() {
+    this.submitted = false;
+    this.markForm.reset();
+    this.selectedData=[];
+}
+toggleVideo() {
+  this.videoplayer.nativeElement.play();
+}
+getSantizeUrl(data) {     
+   return this.sanitizer.bypassSecurityTrustResourceUrl(data);
+}
 }
--- frontend/src/app/users-details/users-details.service.ts
+++ frontend/src/app/users-details/users-details.service.ts
@@ -11,4 +11,10 @@ export class UsersDetailsService {
   getRegistrationProfiles(): Observable < any > {
     return this.httpClient.get(environment.baseUrl + '/getRegistrationProfiles')
   }
+  getjudges(): Observable < any >{
+    return this.httpClient.get(environment.baseUrl+'/getJudgesList')
+  }
+  postMarks(data): Observable < any >{    
+    return this.httpClient.post('http://localhost:3000/api/users/register',data);
+  }
 }
