a44ca8bf7b8d3a2a77953c7303303dbc48af3202
sirishaasapu <asapusirisha@gmail.com>
2021-12-16 20:04:00 +0530

    changes bestguru div

--- frontend/src/app/kuchipudi-event2021/kuchipudi-event2021.component.ts
+++ frontend/src/app/kuchipudi-event2021/kuchipudi-event2021.component.ts
@@ -7,7 +7,7 @@ import { KuchipudiEvent2021Service } from './kuchipudi-event2021.service';
   styleUrls: ['./kuchipudi-event2021.component.css']
 })
 export class KuchipudiEvent2021Component implements OnInit {
-   
+
   // eventData=[{
   //   img:"../../assets/images/5.jpeg",
   //   name:"Gopala Krishna"
@@ -40,13 +40,24 @@ export class KuchipudiEvent2021Component implements OnInit {
   //   name:"Gopala Krishna"
   // }]
   TeacherData;
-  constructor(private kuchipudievent:KuchipudiEvent2021Service) { }
+  bestGurus: any = [];
+  uttamaAcharya:any=[];
+  constructor(private kuchipudievent: KuchipudiEvent2021Service) { }
 
   ngOnInit(): void {
-     this.kuchipudievent.getBestTeachersData().subscribe((data:any)=>
-      {this.TeacherData=data.records
+    this.kuchipudievent.getBestTeachersData().subscribe((data: any) => {
+      this.TeacherData = data.records
       // console.log(this.TeacherData)
+      this.TeacherData.forEach((teacherdata: any) => {
+        if (teacherdata.fields && (teacherdata.fields.TITLE == 'sikamani' || teacherdata.fields.TITLE == 'yesaswi') && teacherdata.fields['AWARD PHOTOS'] && teacherdata.fields['AWARD PHOTOS'][0].url && teacherdata.fields['AWARD PHOTOS'][0].url.length > 0) {
+          this.bestGurus.push(teacherdata);
+        }
+        // else if(teacherdata.fields && teacherdata.fields.TITLE == 'tapaswi' && teacherdata.fields['AWARD PHOTOS'] && teacherdata.fields['AWARD PHOTOS'][0].url && teacherdata.fields['AWARD PHOTOS'][0].url.length > 0 )
+        // {
+        //   this.uttamaAcharya.push(teacherdata);
+        // }
       });
+    });
   }
 
 }
