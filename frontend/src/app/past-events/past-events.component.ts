7c88ddafdf8deef9b6439d7b523b6100ac42dc00
sirishaasapu <asapusirisha@gmail.com>
2021-12-23 12:18:00 +0530

    changes in past event component

--- frontend/src/app/past-events/past-events.component.ts
+++ frontend/src/app/past-events/past-events.component.ts
@@ -7,7 +7,8 @@ import { PastEventsService } from './past-events.service';
   styleUrls: ['./past-events.component.css']
 })
 export class PastEventsComponent implements OnInit {
-
+ 
+  loading=true;
   pastData:any;
   pastEvent2019:any=[];
   pastEvent2020:any=[];
@@ -18,10 +19,14 @@ export class PastEventsComponent implements OnInit {
   constructor(private pastEventData:PastEventsService) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 5000);
     this.pastEventData.getPastEvents().subscribe((data: any) => {
-      this.pastMainData=data;
+     // this.pastMainData=data;
       this.pastData=data.records;
       this.pastData.forEach((PastData: any) => {
+        //console.log(PastData.fields.main)
         if(PastData.fields && (PastData.fields.year == '2019' || PastData.fields.year == '2019to2020' || PastData.fields.year =='2019-20'|| PastData.fields.year == '1st janury' || PastData.fields.year == '3rd janury' ) && PastData.fields.photos && PastData.fields.photos[0].url && PastData.fields.photos[0].url.length > 0)
         {
           this.pastEvent2019.push(PastData);
@@ -34,18 +39,15 @@ export class PastEventsComponent implements OnInit {
         {
           this.pastEvent2021.push(PastData);
         }
-        else if(PastData.fields && PastData.fields.main && PastData.fields.main[0].url && PastData.fields.main[0].url.length > 0)
+      }); 
+      this.pastData.forEach((PastData: any) => {
+         if(PastData.fields && PastData.fields.main && PastData.fields.main[0].url && PastData.fields.main[0].url.length > 0 && PastData.fields.Name && PastData.fields.Name.length > 0)
         {
           this.pastMainData.push(PastData);
+          console.log(this.pastMainData);
         }
-        else if(PastData.fields && PastData.fields.Name && PastData.fields.Name.length > 0)
-        {
-          this.pastDesData.push(PastData);
-        }
-        
       });
+      
   });
 }
-
-
 }
