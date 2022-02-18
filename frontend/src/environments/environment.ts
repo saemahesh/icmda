4c2e7aa0ad4cd736237451bcabfee9115ba3d987
Rajesh Mandapati <stevenrajesh123@gmail.com>
2022-02-18 21:16:46 +0530

    production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl : 'http://localhost:3000/api/users',
   apiUrl : 'http://localhost:3000/api'
 };
