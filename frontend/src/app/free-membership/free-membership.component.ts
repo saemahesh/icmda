543dd0f5112cf7750e79cfb9945cde632efbce2e
Sai Rambha <sai.rambha@factset.com>
2022-02-26 17:51:59 +0530

    changes

--- frontend/src/app/free-membership/free-membership.component.ts
+++ frontend/src/app/free-membership/free-membership.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FreeMembershipComponent implements OnInit {
 
+  loading = true;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 5000);
+  
   }
-
 }
