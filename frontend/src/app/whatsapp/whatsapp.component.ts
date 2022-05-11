3329fc687a550f943806147fdf774aef10251fb0
Sai Rambha <sai.rambha@factset.com>
2022-05-11 19:43:15 +0530

    whatsapp link update

--- frontend/src/app/whatsapp/whatsapp.component.ts
+++ frontend/src/app/whatsapp/whatsapp.component.ts
@@ -10,7 +10,7 @@ export class WhatsappComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.location.href = 'https://chat.whatsapp.com/I1NzArFbkHu08iaKYYVqPI';
+    window.location.href = 'https://chat.whatsapp.com/Efh7eo2WwV3CZIoBWLaHXg';
   }
 
 }
