567bb413e8974106c66f0f4152e14f0f7f71b877
unknown <saemahesh@gmail.com>
2022-06-23 11:14:09 +0530

    monthly-competitions

---
+++ frontend/src/app/monthly-competitions/balamurali-krishna/balamurali-krishna.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-balamurali-krishna',
+  templateUrl: './balamurali-krishna.component.html',
+  styleUrls: ['./balamurali-krishna.component.css']
+})
+export class BalamuraliKrishnaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
