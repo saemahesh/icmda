17f81f650a913141d2f50c2da89e0a140aa8a213
unknown <saemahesh@gmail.com>
2022-09-28 13:45:31 +0530

    photos added

--- frontend/src/app/whatsapp/whatsapp.component.ts
+++ frontend/src/app/whatsapp/whatsapp.component.ts
@@ -12,7 +12,7 @@ export class WhatsappComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.location.href = 'https://chat.whatsapp.com/Efh7eo2WwV3CZIoBWLaHXg';
+    window.location.href = 'https://chat.whatsapp.com/DUZpnPMsDXxKyiQ9GujfYn';
   }
 
 }
